a846fc6e7059e6aed4564f588d04e5f7f7ff3146
Cyrus Zapanza <czapanza@pcso.gov.ph>
2022-06-02 11:27:43 +0800

    Registration

--- MAP/Program.cs
+++ MAP/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using MAP;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
 
